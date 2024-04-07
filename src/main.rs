0ff08d391baf300b13ef24cde41c3d3fa7139ba0
Ethan Shiota <54644546+Piggggggggggy@users.noreply.github.com>
2024-04-06 17:16:29 -0700

    now filters out urls

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::ops::RangeBounds;
+
 use arboard::Clipboard;
 use eframe::{
     egui::{self, Layout, Widget},
     epaint::vec2,
 };
-use uwuifier::uwuify_str_sse;
+use regex::Regex;
+use uwuifier::{round_up16, uwuify_sse, uwuify_str_sse};
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
@@ -42,11 +45,41 @@ impl MyApp {
             self.text = text;
         }
     }
+
+    fn uwuify(&mut self) {
+        let b = &self.text;
+        // filter out urls
+        let url_regex = regex::Regex::new(r"https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)").unwrap();
+        let non_url_text = url_regex.split(b).collect::<Vec<_>>();
+        let mut matches = url_regex.captures_iter(b).peekable();
+
+        let mut temp1 = vec![0u8; round_up16(b.len()) * 16];
+        let mut temp2 = vec![0u8; round_up16(b.len()) * 16];
+        let mut text = non_url_text
+            .iter()
+            .map(|s| {
+                String::from_utf8_lossy(uwuify_sse(s.as_bytes(), &mut temp1, &mut temp2))
+                    .to_string()
+            })
+            .peekable();
+
+        let mut result = String::new();
+        while matches.peek().is_some() || text.peek().is_some() {
+            if let Some(s) = text.next() {
+                result.push_str(&s);
+            };
+            if let Some(s) = matches.next() {
+                result.push_str(s.get(0).unwrap().as_str());
+            };
+        }
+        self.text = result;
+    }
 }
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let default_button = |e: &str| egui::Button::new(e).min_size(vec2(20.0, 30.0));
         egui::CentralPanel::default().show(ctx, |ui| {
+            // main vertical layout
             ui.vertical(|ui| {
                 let (_id, rect) = ui.allocate_space(vec2(ui.available_width(), 30.0));
                 // text
@@ -59,7 +92,7 @@ impl eframe::App for MyApp {
                 ui.put(rect, |ui: &mut egui::Ui| {
                     ui.horizontal(|ui: &mut egui::Ui| {
                         if default_button("uwuify").ui(ui).clicked() {
-                            self.text = uwuify_str_sse(&self.text);
+                            self.uwuify();
                         }
                         ui.allocate_ui_with_layout(
                             ui.available_size(),
